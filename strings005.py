272d077181437c3b772abef983aa05eccfed2fd8
Dil2023 <dilermando_macedo@hotmail.com>
2023-07-06 21:27:20 -0300

    final problem add that analyzes if a string is an anagram of a palindrome

---
+++ strings005.py
@@ -0,0 +1,22 @@
+input = "racecar"
+
+def is_anagram_of_palindrome(s):
+    char_count = {}
+
+    for char in s:
+        if char not in char_count:
+            char_count[char] = 1
+        else:
+            char_count[char] += 1
+
+    odd_count = 0
+    for count in char_count.values():
+        if count % 2 != 0:
+            odd_count += 1
+
+        if odd_count > 1:
+            return False
+
+    return True
+
+print(is_anagram_of_palindrome(input))
